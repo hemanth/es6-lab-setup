21220083a6761fadd5ff6136d606247196192eb7
Ashutosh Agrawal <ashutosh@hackerrank.com>
2014-05-24 15:58:57 +0530

    counters in es6

--- spec/test.js
+++ spec/test.js
@@ -1,12 +1,13 @@
 var es6 = require("../dist/es6.js");
-var traceur = require('traceur');
-$traceurRuntime = traceur.runtime;
 describe("ES6 suite", function() {
   it("should sq the number", function() {
     expect(es6.arrow(2)).toBe(4);
   });
   it("should give a count value", function() {
-     var countr = new es6.gen();
-     //expect()
+     var countr = new(new es6.gen());
+     expect(countr.next().value).toBe(0);
+     expect(countr.next().value).toBe(1);
+     expect(countr.next().value).toBe(2);
+     expect(countr.next().value).toBe(3);
   });
 });
